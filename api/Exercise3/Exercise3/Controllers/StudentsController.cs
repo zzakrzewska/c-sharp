13c11a139519b7ffe4ebf5422ff937ddc467a9ba
zzakrzewska <95741647+zzakrzewska@users.noreply.github.com>
2023-03-29 10:02:03 +0200

    add

---
+++ api/Exercise3/Exercise3/Controllers/StudentsController.cs
@@ -0,0 +1,92 @@
+﻿using Exercise3.Models;
+using Exercise3.Models.DTOs;
+using Exercise3.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentsRepository _studentsRepository;
+        public StudentsController(IStudentsRepository studentsRepository)
+        {
+            _studentsRepository = studentsRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(_studentsRepository.GetStudents());
+            }
+            catch (Exception e)
+            {
+                return Problem();
+            }
+        }
+
+        [HttpGet("{index}")]
+        public async Task<IActionResult> Get(string index)
+        {
+            try
+            {
+                var student = _studentsRepository.GetStudents().Where(e => e.IndexNumber == index).FirstOrDefault();
+                if (student is null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(student);
+            }
+            catch (Exception e)
+            {
+                return Problem();
+            }
+
+        }
+
+        [HttpPut("{index}")]
+        public async Task<IActionResult> Put(string index, StudentPUT newStudentData)
+        {
+            try
+            {
+                var student = _studentsRepository.GetStudents().Where(e => e.IndexNumber == index).FirstOrDefault();
+                if (student is null)
+                {
+                    return NotFound();
+                }
+
+                _studentsRepository.UpdateStudent(student,
+                    new Student
+                    {
+                        FirstName = newStudentData.FirstName,
+                        LastName = newStudentData.LastName,
+                        //uzup
+                    }
+                );
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return Problem();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(StudentPOST newStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{index}")]
+        public async Task<IActionResult> Delete(string index)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
